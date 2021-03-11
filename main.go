47facbb85980c05b9ffb90859f8d7d799fa162f8
chenbb <2636966033@qq.com>
2021-03-11 14:46:55 +0800

    no message

--- main.go
+++ main.go
@@ -8,18 +8,19 @@ import (
 
 // error handling left out for brevity
 func main() {
+	fmt.Println("hello git diff")
+	fmt.Println("hello git diff")
+	fmt.Println("hello git diff")
 	byt, _ := ioutil.ReadFile("example.diff")
 	diff, _ := diffparser.Parse(string(byt))
 
 	// You now have a slice of files from the diff,
 	file := diff.Files[0]
-
 	// diff hunks in the file,
 	hunk := file.Hunks[0]
 
 	// new and old ranges in the hunk
 	newRange := hunk.NewRange
-
 	// and lines in the ranges.
 	line := newRange.Lines[0]
 	fmt.Println("line=============", line)
