acf2e2d18396a694f9bcc23e3580fb28ca4663a0
chenbb <2636966033@qq.com>
2021-03-11 14:27:33 +0800

    init

---
+++ diffparser/diffparser_test.go
@@ -0,0 +1,134 @@
+package diffparser
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TODO(waigani) tests are missing more creative names (spaces, special
+// chars), and diffed files that are not in the current directory.
+
+func setup(t *testing.T) *Diff {
+	byt, err := ioutil.ReadFile("example.diff")
+	require.NoError(t, err)
+
+	diff, err := Parse(string(byt))
+	require.NoError(t, err)
+	require.Equal(t, len(diff.Files), 6)
+
+	return diff
+}
+func TestFileModeAndNaming(t *testing.T) {
+	diff := setup(t)
+	for i, expected := range []struct {
+		mode     FileMode
+		origName string
+		newName  string
+	}{
+		{
+			mode:     MODIFIED,
+			origName: "file1",
+			newName:  "file1",
+		},
+		{
+			mode:     DELETED,
+			origName: "file2",
+			newName:  "",
+		},
+		{
+			mode:     DELETED,
+			origName: "file3",
+			newName:  "",
+		},
+		{
+			mode:     NEW,
+			origName: "",
+			newName:  "file4",
+		},
+		{
+			mode:     NEW,
+			origName: "",
+			newName:  "newname",
+		},
+		{
+			mode:     DELETED,
+			origName: "symlink",
+			newName:  "",
+		},
+	} {
+		file := diff.Files[i]
+		t.Logf("testing file: %v", file)
+		require.Equal(t, expected.mode, file.Mode)
+		require.Equal(t, expected.origName, file.OrigName)
+		require.Equal(t, expected.newName, file.NewName)
+	}
+}
+
+func TestHunk(t *testing.T) {
+	diff := setup(t)
+	expectedOrigLines := []DiffLine{
+		{
+			Mode:     UNCHANGED,
+			Number:   1,
+			Content:  "some",
+			Position: 2,
+		}, {
+			Mode:     UNCHANGED,
+			Number:   2,
+			Content:  "lines",
+			Position: 3,
+		}, {
+			Mode:     REMOVED,
+			Number:   3,
+			Content:  "in",
+			Position: 4,
+		}, {
+			Mode:     UNCHANGED,
+			Number:   4,
+			Content:  "file1",
+			Position: 5,
+		},
+	}
+
+	expectedNewLines := []DiffLine{
+		{
+			Mode:     ADDED,
+			Number:   1,
+			Content:  "add a line",
+			Position: 1,
+		}, {
+			Mode:     UNCHANGED,
+			Number:   2,
+			Content:  "some",
+			Position: 2,
+		}, {
+			Mode:     UNCHANGED,
+			Number:   3,
+			Content:  "lines",
+			Position: 3,
+		}, {
+			Mode:     UNCHANGED,
+			Number:   4,
+			Content:  "file1",
+			Position: 5,
+		},
+	}
+
+	file := diff.Files[0]
+	origRange := file.Hunks[0].OrigRange
+	newRange := file.Hunks[0].NewRange
+
+	require.Equal(t, 1, origRange.Start)
+	require.Equal(t, 4, origRange.Length)
+	require.Equal(t, 1, newRange.Start)
+	require.Equal(t, 4, newRange.Length)
+
+	for i, line := range expectedOrigLines {
+		require.Equal(t, line, *origRange.Lines[i])
+	}
+	for i, line := range expectedNewLines {
+		require.Equal(t, line, *newRange.Lines[i])
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"git-diff/diffparser"
+	"io/ioutil"
+)
 
+// error handling left out for brevity
 func main() {
-	fmt.Println("hello  go")
+	byt, _ := ioutil.ReadFile("example.diff")
+	diff, _ := diffparser.Parse(string(byt))
+
+	// You now have a slice of files from the diff,
+	file := diff.Files[0]
+
+	// diff hunks in the file,
+	hunk := file.Hunks[0]
+
+	// new and old ranges in the hunk
+	newRange := hunk.NewRange
+
+	// and lines in the ranges.
+	line := newRange.Lines[0]
+	fmt.Println("line=============", line)
 }
